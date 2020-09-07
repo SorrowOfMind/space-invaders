2a9a8f9886868a4a497728f57450f954affdddc0
Basia <b.kumor@yahoo.com>
2020-09-07 18:47:45 +0200

    initiate player, controller

--- game.js
+++ game.js
@@ -4,6 +4,12 @@ const ctx = cvs.getContext('2d');
 const width = ctx.canvas.width;
 const height = ctx.canvas.height;
 
+const BOX = 10;
+
+const bg = new Image();
+bg.src = './assets/bg.png';
+
+
 class Invader {
     constructor(x, y) {
         this.x = x,
@@ -11,3 +17,61 @@ class Invader {
         this.r = 0
     }
 }
+
+class Player {
+    constructor(x,y,w,h) {
+        this.x = x,
+        this.y = y,
+        this.w = w,
+        this.h = h,
+        this.color = "green"
+    }
+
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.w, this.h)
+    }
+}
+
+let player = new Player(width*0.5 - BOX*2, height*0.9, BOX*2, BOX*4);
+
+const controller = {
+    right: false,
+    left: false,
+
+    checkKeys(e) {
+        let state = e.type === 'keydown' ? true : false;
+        let key = e.keyCode;
+
+        switch(key) {
+            case 37:
+                controller.left = state;
+                break;
+            case 39:
+                controller.right = state;
+                break;
+        }
+    }
+}
+
+const createInvaders = () => {
+    for(let i = 0; i < 20; i++) {
+
+    }
+}
+
+
+bg.addEventListener('load', function() {
+    ctx.drawImage(this, 0, 0);
+})
+
+let gameLoop = () => {
+    player.draw();
+
+    window.requestAnimationFrame(gameLoop)
+}
+
+
+window.addEventListener('keydown', controller.checkKeys);
+window.addEventListener('keyup', controller.checkKeys);
+window.requestAnimationFrame(gameLoop);
